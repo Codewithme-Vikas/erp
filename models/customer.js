c339096d4340aff6f26ad1f6a86bf8076b66d021
Vikas <104128474+Codewithme-Vikash@users.noreply.github.com>
2024-02-28 16:30:42 +0530

    Add files via upload
    remove email : required from customer model

--- models/customer.js
+++ models/customer.js
@@ -4,7 +4,7 @@ const customerschema = new mongoose.Schema({
     name : {type:String, required : true},
     add : {type:String, required : true},
     mob : {type:Number, required : true},
-    email : {type:String, required : true},
+    email : {type:String},
     gst : {type:String,},
     pan : {type:String,}
 })
