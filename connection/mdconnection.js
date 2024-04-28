d7f0340114b522570bf0d07b7d68ab5e44f7ec6e
Vikas <104128474+Codewithme-Vikas@users.noreply.github.com>
2024-04-28 10:35:21 +0530

    Update mdconnection.js

--- connection/mdconnection.js
+++ connection/mdconnection.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const mdconnection = () => {
 
-    // mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.go1g8kd.mongodb.net/erpDB`)
-    mongoose.connect(process.env.MONGO_URL)
+    // mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.go1g8kd.mongodb.net/erpDB`)
         .then(() => console.log("DB connected successfully!"))
         .catch((err) => {
             console.log("DB is not connected!")
